5a59446e5fcfc7383cb3cf12f14c97b50aaefa49
timothybaxter <91533436+timothybaxter@users.noreply.github.com>
2025-01-05 18:30:26 +0000

    second test for the webhooks

--- auth-lambda-local/index.mjs
+++ auth-lambda-local/index.mjs
@@ -4,7 +4,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 // Add this near the start of your handler function
 console.log('Auth Lambda function executed - PIPELINE TEST');
-console.log('Testing automatic deployment via webhook - ' + new Date().toISOString());
+console.log('Testing automatic deployment via webhook - test 2' + new Date().toISOString());
 
 // User Schema
 const userSchema = new mongoose.Schema({
