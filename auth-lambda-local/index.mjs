b75a1788564ab5cb84529e5812db7a8bc57d9b0e
timothybaxter <91533436+timothybaxter@users.noreply.github.com>
2025-01-10 15:39:54 +0000

    pipeline test

--- auth-lambda-local/index.mjs
+++ auth-lambda-local/index.mjs
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
+console.log("Pipeline test");
+
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
